1b5e643b5fd74326d3590f91dd6d966593a953d1
Vishnupriya <vishnupriya.t7@gmail.com>
2023-09-14 14:03:33 +0530

    finance_committee_report-approval-completed

--- contest-bkrvw-regstrn-sample.php
+++ contest-bkrvw-regstrn-sample.php
@@ -3,6 +3,14 @@
 
 <?php include "head-style.php"; ?>
 
+<style>
+    /* Additional CSS for adjusting the card width */
+    .card {
+        max-width: 100vw;
+        /* Allow the card to expand to its container's width */
+    }
+</style>
+
 <body>
 
     <!-- ======= Header ======= -->
@@ -26,20 +34,49 @@
             </div>
         </section><!-- End Breadcrumbs Section -->
 
-        <section class="contest-bkrvw-reg">
-            <div class="container">
+        <!-- <section class="contest-bkrvw-reg"> -->
+        <!-- <div class="container">
                 <div class="row">
-                    <!-- <div class="col-12"> -->
+                    <div class="col-12">
                         <div class="card overflow-auto">
                             <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScKw20kdrJw7rUjexrK_vo8HU4-mIN3M1NT7wAXfSbFYrph9w/viewform?embedded=true" width="640" height="1815" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
                         </div>
-                    <!-- </div> -->
-
-
+                     </div> 
 
                 </div>
-            </div>
-        </section>
+            </div> -->
+        <!-- <div class="container d-flex justify-content-center align-items-center min-vh-100"> -->
+        <!-- Section to hold your Google Sheets link -->
+        <!-- <section> -->
+        <!-- <h1>Your Page Heading</h1> -->
+        <!-- <p>Your page content here...</p> -->
+        <!-- Replace the Google Sheets link with your actual link -->
+        <!-- <a href="https://docs.google.com/spreadsheets/d/YourSheetID" target="_blank">Google Sheets Link</a> -->
+        <!-- <div class="card overflow-auto">
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScKw20kdrJw7rUjexrK_vo8HU4-mIN3M1NT7wAXfSbFYrph9w/viewform?embedded=true" width="640" height="1815" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                </div> -->
+        <!-- </section> -->
+        <!-- </div>
+        </section> -->
+        <div class="container d-flex justify-content-center align-items-center min-vh-100">
+            <!-- Section to hold your Google Sheets link -->
+            <section>
+                <h1>Your Page Heading</h1>
+                <p>Your page content here...</p>
+                <!-- <div class="col-12">
+                    <div class="card overflow-auto">
+                        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScKw20kdrJw7rUjexrK_vo8HU4-mIN3M1NT7wAXfSbFYrph9w/viewform?embedded=true" width="100%" height="600vh" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                    </div>
+                </div> -->
+
+                <div class="col-12 col-md-6">
+                    <!-- <iframe class="mb-4 mb-lg-0" src="https://maps.google.com/maps?width=588&amp;height=456&amp;hl=en&amp;q=Kerala Niyamasabha&amp;t=&amp;z=18&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen></iframe> -->
+                    <!-- <iframe src="https://maps.google.com/maps?width=588&amp;height=456&amp;hl=en&amp;q=Kerala Niyamasabha&amp;t=&amp;z=18&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" width="100" height="100" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScKw20kdrJw7rUjexrK_vo8HU4-mIN3M1NT7wAXfSbFYrph9w/viewform?embedded=true"  frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+
+                </div>
+            </section>
+        </div>
 
     </main><!-- End #main -->
 
