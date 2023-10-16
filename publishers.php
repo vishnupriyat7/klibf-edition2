2a16e882fae4127690499b39d2f366c4946ada5a
Vishnupriya <vishnupriya.t7@gmail.com>
2023-10-16 15:38:00 +0530

    publisher-page pagination-completed

--- publishers.php
+++ publishers.php
@@ -9,7 +9,30 @@
     margin-left: auto;
     margin-right: auto;
   }
+
+  .container {
+    text-align: center;
+  }
+
+  .pagination {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    /* padding: 10px; */
+    border-radius: 5px;
+  }
+
+  .pagination a {
+    background: rgba(111, 153, 32, 0.9);
+    color: black;
+    float: left;
+    padding: 8px 16px;
+    text-decoration: none;
+  }
 </style>
+
+
 
 <body>
 
@@ -46,7 +69,7 @@
           </thead>
           <tbody>
             <?php
-            $query = "SELECT up.id, up.user_id, up.org_name, up.logo, ch.user_id, ch.status FROM users_profile up JOIN challan ch ON up.user_id = ch.user_id WHERE ch.status = 'A' ORDER BY up.id DESC";
+            $query = "SELECT up.id, up.user_id, up.org_name, up.logo, ch.user_id, ch.status FROM users_profile up JOIN challan ch ON up.user_id = ch.user_id WHERE ch.status = 'A' ORDER BY up.org_name ASC";
             // $query = "SELECT up.*, sb.*, ch.user_id, ch.bank_name, ch.paid_amt, ch.trnctn_no, ch.trnctn_type, ch.trnctn_date, ch.paye_name, ch.ifsc, ch.status, ch.updated_date, ch.invoice_no, ch.id as chid FROM users_profile up JOIN stall_booking sb ON up.user_id = sb.user_id JOIN challan ch ON up.user_id = ch.user_id ORDER BY up.id DESC";
             $publshrdetls = mysqli_query($conn, $query);
             // var_dump($publshrdetls);
@@ -73,8 +96,9 @@
           </tbody>
         </table>
       </div>
-      <div class="pagination">
+      <div class="pagination " id="pagination">
         <a id="prev" href="#">Previous</a>
+        <div id="pageNumbers"></div>
         <a id="next" href="#">Next</a>
       </div>
     </section>
@@ -87,9 +111,9 @@
 
   <script>
     $(document).ready(function() {
-      // Set the number of rows to display per page
       var rowsPerPage = 10;
       var currentPage = 1;
+      var totalRows = $('#example tbody tr').length;
 
       function showPage(page) {
         var tableRows = $('#example tbody tr');
@@ -97,8 +121,26 @@
         tableRows.slice((page - 1) * rowsPerPage, page * rowsPerPage).show();
       }
 
+      function generatePageNumbers() {
+        var totalPages = Math.ceil(totalRows / rowsPerPage);
+        var pageNumbersHtml = '';
+
+        for (var i = 1; i <= totalPages; i++) {
+          pageNumbersHtml += '<a class="page" href="#" data-page="' + i + '">' + i + '</a>';
+        }
+
+        $('#pageNumbers').html(pageNumbersHtml);
+      }
+
+      function updatePagination() {
+        generatePageNumbers();
+        $('.page').removeClass('active');
+        $('.page[data-page="' + currentPage + '"]').addClass('active');
+      }
+
       // Initial display
       showPage(currentPage);
+      updatePagination();
 
       // Previous page
       $('#prev').click(function(e) {
@@ -106,15 +148,28 @@
         if (currentPage > 1) {
           currentPage--;
           showPage(currentPage);
+          updatePagination();
         }
       });
 
       // Next page
       $('#next').click(function(e) {
         e.preventDefault();
-        if (currentPage < Math.ceil($('#example tbody tr').length / rowsPerPage)) {
+        if (currentPage < Math.ceil(totalRows / rowsPerPage)) {
           currentPage++;
           showPage(currentPage);
+          updatePagination();
+        }
+      });
+
+      // Click on page number
+      $(document).on('click', '.page', function(e) {
+        e.preventDefault();
+        var newPage = $(this).data('page');
+        if (newPage !== currentPage) {
+          currentPage = newPage;
+          showPage(currentPage);
+          updatePagination();
         }
       });
     });
