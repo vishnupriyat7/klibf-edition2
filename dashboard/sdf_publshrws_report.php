a3845f4d805b23a52d131ed253b04f21e601e670
Vishnupriya <vishnupriya.t7@gmail.com>
2023-11-29 16:52:46 +0530

    sdf-coupon-details-processing

--- dashboard/sdf_publshrws_report.php
+++ dashboard/sdf_publshrws_report.php
@@ -84,9 +84,9 @@ function convertNumberToWordsForIndia($number)
 <!-- ============================================================== -->
 <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> -->
 <style>
-    #pay-slip td {
+    #example td {
         text-align: right !important;
-        width: 20%;
+        width: 10%;
     }
 </style>
 
@@ -158,6 +158,7 @@ function convertNumberToWordsForIndia($number)
 
                                     // $querycoupon = "SELECT cp.*, cb.*  FROM coupon_publisher cp JOIN  coupon_bankdtls cb ON cp.users_id = cb.users_id WHERE cp.users_id = $userId ORDER BY cp.id DESC";
                                     $couponlist = mysqli_query($con, $querycoupon);
+                                    // var_dump($couponlist);
                                     $counter = 0;
                                     while ($coupon = mysqli_fetch_array($couponlist)) {
                                         $id = $coupon['id'];
@@ -171,14 +172,21 @@ function convertNumberToWordsForIndia($number)
                                         $coupon50ct = $coupon['cpn_50_count'];
                                         // $coupon50slno = $coupon['cpn_50_srlno'];
                                         $coupon50amnt = $coupon50ct * 50;
+                                        $formattedCoupon50amnt = number_format($coupon50amnt, 0, '', ',');
+                                        // var_dump($formattedCoupon50amnt);
                                         $coupon100ct = $coupon['cpn_100_count'];
                                         // $coupon100slno = $coupon['cpn_100_srlno'];
                                         $coupon100amnt = $coupon100ct * 100;
+                                        $formattedCoupon100amnt = number_format($coupon100amnt, 0, '', ',');
+                                        // var_dump($formattedCoupon100amnt);
                                         $coupon200ct = $coupon['cpn_200_count'];
                                         // $coupon200slno = $coupon['cpn_200_srlno'];
                                         $coupon200amnt = $coupon200ct * 200;
+                                        $formattedCoupon200amnt = number_format($coupon200amnt, 0, '', ',');
+                                        // var_dump($formattedCoupon200amnt);
                                         // $coupontotalamnt = $coupon['total_amount'];
                                         $coupontotalamnt = $coupon50amnt + $coupon100amnt + $coupon200amnt;
+                                        $formtd_coupontotalamnt = number_format($coupontotalamnt, 0, '', ',');
                                         $bankname = $coupon['bank_name'];
                                         $account_no = $coupon['account_no'];
                                         $bank_ifsc = $coupon['bank_ifsc'];
@@ -234,7 +242,7 @@ function convertNumberToWordsForIndia($number)
                                                 <?= $bank_ifsc; ?>
                                             </td>
                                             <td>
-                                                <button name="print" class="btn btn-primary" id="print-slip" onclick="printSlip()">Print Payment Slip</button>
+                                                <button name="print" class="btn btn-primary" id="example" onclick="printSlip(<?= $userId; ?>)">Print Payment Slip</button>
                                             </td>
                                         </tr>
                                     <?php } ?>
@@ -257,17 +265,17 @@ function convertNumberToWordsForIndia($number)
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.debug.js" integrity="sha384-NaWTHo/8YCBYJ59830LTz/P4aQZK1sS0SneOgAvhsIl3zBu8r9RevNg5lHCHAuQ/" crossorigin="anonymous"></script>
 <script type="text/javascript">
-    function printSlip() {
+    function printSlip(rowId) {
         var pubName = <?php echo json_encode($pub_name) ?>;
         var head_org_addr = <?php echo json_encode($capitalized_head_org_addr) ?>;
         var cntct_no = <?php echo json_encode($cntct_no) ?>;
         var coupon50ct = <?php echo json_encode($coupon50ct) ?>;
-        var coupon50amnt = <?php echo json_encode($coupon50amnt) ?>;
+        var coupon50amnt = <?php echo json_encode($formattedCoupon50amnt) ?>;
         var coupon100ct = <?php echo json_encode($coupon100ct) ?>;
-        var coupon100amnt = <?php echo json_encode($coupon100amnt) ?>;
+        var coupon100amnt = <?php echo json_encode($formattedCoupon100amnt) ?>;
         var coupon200ct = <?php echo json_encode($coupon200ct) ?>;
-        var coupon200amnt = <?php echo json_encode($coupon200amnt) ?>;
-        var coupontotalamnt = <?php echo json_encode($coupontotalamnt) ?>;
+        var coupon200amnt = <?php echo json_encode($formattedCoupon200amnt) ?>;
+        var coupontotalamnt = <?php echo json_encode($formtd_coupontotalamnt) ?>;
         var bankname = <?php echo json_encode($bankname) ?>;
         var account_no = <?php echo json_encode($account_no) ?>;
         var bank_ifsc = <?php echo json_encode($bank_ifsc) ?>;
@@ -275,36 +283,38 @@ function convertNumberToWordsForIndia($number)
         var totalinword = <?php echo json_encode($totalinword) ?>;
         // ... (other variables)
 
-        var htmlContent = '<html><head><style>' +
-            '@media print {' +
-            '   table, th, td {' +
-            '       border: none !important;' +
-            '   }' +
-            '   body {' +
-            '       font-size: 10pt;' +
-            '   }' +
-            '}</style>' +
-            '<link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />' +
-            '<link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />' +
-            '<link href="assets/css/app.min.css" rel="stylesheet" type="text/css" />' +
-            '<link href="assets/css/custom.min.css" rel="stylesheet" type="text/css" />' +
-            '</head><body><br><label><img src="assets/images/Logo_01.png" height="70vh" style="float: left;"></label>' +
-            '<h3><b>PAYMENT DETAILS</b></h3><br><h4><b>Name of Publisher:&nbsp;&nbsp;' + pubName + '</h4>' +
-            '<br><h4><b>Address:&nbsp;&nbsp;' + head_org_addr + '</h4><br><h4><b>Contact No:&nbsp;&nbsp;' + cntct_no + '</h4>' +
-            '<br><h3><b>AMOUNT DETAILS</b></h3><table class="table table-info table-responsive" id="pay-slip">' +
-            '<tr><th class="text-center">Denomination</th><th class="text-center">Count</th><th class="text-center">Amount(in ₹).</th></tr>' +
-            '<tbody><tr><th class="text-center">50</th><td class="text-center">' + coupon50ct + '</td>' +
-            '<td class="text-center">' + coupon50amnt + '</td></tr><tr><th class="text-center">100</th>' +
-            '<td class="text-center">' + coupon100ct + '</td><td class="text-center">' + coupon100amnt + '</td></tr>' +
-            '<tr><th class="text-center">200</th><td class="text-center">' + coupon200ct + '</td>' +
-            '<td class="text-center">' + coupon200amnt + '</td></tr><tr><td colspan="2">Total amount payable (in ₹&nbsp;).</td>' +
-            '<td><b>' + coupontotalamnt + '</b></td></tr><tr><td>Total amount payable (in words).</td>' +
-            '<td><b>' + totalinword + '</b></td></tr></tbody></table><br><h4><b><u>Details of bank account to which payment is made:</u></b></h4>' +
-            '<table class="table table-info table-responsive" id="pay-slip"><tr><td><b>Bank Name:</b></td>' +
-            '<td>' + bankname + '</td></tr><tr><td><b>Branch:</b></td><td>' + bank_branch + '</td></tr>' +
-            '<tr><td><b>Account Number:</b></td><td>' + account_no + '</td></tr><tr><td><b>IFSC</b></td>' +
-            '<td colspan="1">' + bank_ifsc + '</td></tr></tbody></table><br><br>' +
-            '<h4 style="float: end; text-align: right;"><b>VERIFIED</b></h4></body></html>';
+        // var htmlContent = '<html><head><style>' +
+        //     '@media print {' +
+        //     '   table, th, td {' +
+        //     '       border: none !important;' +
+        //     '   }' +
+        //     '   body {' +
+        //     '       font-size: 10pt;' +
+        //     '   }' +
+        //     '}</style>' +
+        //     '<link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />' +
+        //     '<link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />' +
+        //     '<link href="assets/css/app.min.css" rel="stylesheet" type="text/css" />' +
+        //     '<link href="assets/css/custom.min.css" rel="stylesheet" type="text/css" />' +
+        //     '</head><body><br><label><img src="assets/images/Logo_01.png" height="70vh" style="float: left;"></label>' +
+        //     '<h3><b>PAYMENT DETAILS</b></h3><br><h4><b>Name of Publisher:&nbsp;&nbsp;' + pubName + '</h4>' +
+        //     '<br><h4><b>Address:&nbsp;&nbsp;' + head_org_addr + '</h4><br><h4><b>Contact No:&nbsp;&nbsp;' + cntct_no + '</h4>' +
+        //     '<br><h3><b>AMOUNT DETAILS</b></h3><table class="table table-info table-responsive" id="example">' +
+        //     '<tr><th class="text-center">Denomination</th><th class="text-center">Count</th><th class="text-center">Amount(in ₹).</th></tr>' +
+        //     '<tbody><tr><th class="text-center">50</th><td class="text-center">' + coupon50ct + '</td>' +
+        //     '<td class="text-center">' + coupon50amnt + '</td></tr><tr><th class="text-center">100</th>' +
+        //     '<td class="text-center">' + coupon100ct + '</td><td class="text-center">' + coupon100amnt + '</td></tr>' +
+        //     '<tr><th class="text-center">200</th><td class="text-center">' + coupon200ct + '</td>' +
+        //     '<td class="text-center">' + coupon200amnt + '</td></tr><tr><td colspan="2">Total amount payable (in ₹&nbsp;).</td>' +
+        //     '<td><b>' + coupontotalamnt + '</b></td></tr><tr><td>Total amount payable (in words).</td>' +
+        //     '<td><b>' + totalinword + '</b></td></tr></tbody></table><br><h4><b><u>Details of bank account to which payment is made:</u></b></h4>' +
+        //     '<table class="table table-info table-responsive" id="example"><tr><td><b>Bank Name:</b></td>' +
+        //     '<td>' + bankname + '</td></tr><tr><td><b>Branch:</b></td><td>' + bank_branch + '</td></tr>' +
+        //     '<tr><td><b>Account Number:</b></td><td>' + account_no + '</td></tr><tr><td><b>IFSC</b></td>' +
+        //     '<td colspan="1">' + bank_ifsc + '</td></tr></tbody></table><br><br>' +
+        //     '<h4 style="float: end; text-align: right;"><b>VERIFIED</b></h4></body></html>';
+
+        var htmlContent = '<html><head> <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" /><link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" /><link href="assets/css/app.min.css" rel="stylesheet" type="text/css" /><link href="assets/css/custom.min.css" rel="stylesheet" type="text/css" /></head><body><br><label><img src="assets/images/Logo_01.png" height="70vh" style="float: left;"></label><h3><b>PAYMENT DETAILS</b></h3><br><h4><b>Name of Publisher:&nbsp;&nbsp;' + pubName + '</h4><br><h4><b>Address:&nbsp;&nbsp;' + head_org_addr + '</h4><br><h4><b>Contact No:&nbsp;&nbsp;'+ cntct_no +'</h4><br><h3><b>AMOUNT DETAILS</b></h3><table class="table table-info table-responsive" id="example"><tr><th class="text-center">Denomination</th><th class="text-center">Count</th><th class="text-center">Amount</th></tr><tbody><tr><th class="text-center">50</th><td class="text-center">' + coupon50ct + '</td><td class="text-center">' + coupon50amnt + '</td></tr><tr><th class="text-center">100</th><td class="text-center">' + coupon100ct + '</td><td class="text-center">' + coupon100amnt + '</td></tr><tr><th class="text-center">200</th><td class="text-center">' + coupon200ct + '</td><td class="text-center">' + coupon200amnt + '</td></tr><tr><td colspan="2">Total amount payable (in ₹&nbsp;).</td><td><b>' + coupontotalamnt + '</b></td></tr><tr><td>Total amount payable (in words).</td><td><b>' + totalinword + '</b></td></tr></tbody></table><br><h4><b><u>Details of bank account to which payment is made:</u></b></h4><table class="table table-info table-responsive" id="example"><tr><td><b>Bank Name:</b></td><td>' + bankname + '</td></tr><tr><td><b>Branch:</b></td><td>' + bank_branch + '</td></tr><tr><td><b>Account Number:</b></td><td>' + account_no + '</td></tr><tr><td><b>IFSC</b></td><td colspan="1">' + bank_ifsc + '</td></tr></tbody></table><br><br><h4 style="float: end; text-align: right;"><b>VERIFIED</b></h4></body></html>'
 
         var iframe = document.getElementById("print-frame");
         iframe.contentDocument.write(htmlContent);
